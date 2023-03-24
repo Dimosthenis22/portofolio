15a06f864121588396a2c0628945b2a054426519
Dimosthenis22 <dimosthenisk80@gmail.com>
2023-03-24 17:50:29 +0100

    backend done, frontend:About one, header done, work done, skills I'm struggling with an error

---
+++ frontend_react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+// import * as ReactDOMClient from 'react-dom/client';
+
+ReactDOM.createRoot(<App />, document.getElementById('root'));
+
+// ReactDOMClient.createRoot(<App />, document.getElementById('root'));
